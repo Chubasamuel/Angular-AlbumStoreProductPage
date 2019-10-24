0d170de512017e522f8dc8577db0a00cde278bea
Chuba Samuel <chubasamuel@gmail.com>
2019-10-24 22:12:08 +0100

    Adding the Tracklisting component

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+//import {ProductTracklistingComponent} from '../product-tracklisting/product-tracklisting.component'
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
